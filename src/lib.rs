3c04d1b14526b7dbbdb8c21bcd8f5c6c0429da61
Han Kruiger <mail@hankruiger.com>
2023-08-17 11:44:44 +0200

    chore: update dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,15 @@
 extern crate qrcodegen;
 
-use wasm_bindgen::prelude::*;
+mod utils;
+
+use utils::to_svg_string;
+
 use qrcodegen::QrCode;
 use qrcodegen::QrCodeEcc;
+use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn make_qr(url: String, border: i32) -> String {
-  QrCode::encode_text(url.as_str(), QrCodeEcc::Medium)
-    .unwrap()
-    .to_svg_string(border)
+    let qr_code = &QrCode::encode_text(url.as_str(), QrCodeEcc::Medium).unwrap();
+    to_svg_string(&qr_code, border)
 }
